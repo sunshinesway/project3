72874353cf5a58fece315fd7d661e5ecd9cd86b9
Cheyenne C Swasey <cheysway1.git@purl.anonaddy.com>
2023-09-30 13:55:55 -0700

    updated

--- help-PJ1-updated/list_read.cpp
+++ help-PJ1-updated/list_read.cpp
@@ -1,17 +1,19 @@
+// 1227888508
+
 #include <stdio.h>
 #include <string.h>
 #include "list_read.h"
 
-// Function to print the contents of the list to stdout
+// Print the List contents
 void listPrint(LIST *List) {
     NODE *pNODE = List->head;
     while (pNODE) {
-        std::cout << pNODE->key << std::endl;
+        printf("%lf\n", pNODE->key);
         pNODE = pNODE->next;
     }
 }
 
-// Function to find and return the maximum key in the list
+// Find the maximum key in List
 double listMax(LIST *List) {
     double max = List->head->key;
     NODE *pNODE = List->head->next;
--- help-PJ1-updated/list_write.cpp
+++ help-PJ1-updated/list_write.cpp
@@ -1,13 +1,17 @@
+// 1227888508
+
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include "list_write.h"
-#include "util.h"  // Include your custom header files
+#include "util.h"
 
-// Function to insert a new node with the given key at the head of the list
+// Insert a NEW node with the key at the head of List
 void listInsert(LIST *List, double key) {
+    // allocate memory for newNode
     NODE *newNode = (NODE *)malloc(sizeof(NODE));
     if (!newNode) {
-        std::cerr << "Error: malloc failed" << std::endl;
+        fprintf(stderr, "Error: malloc failed\n");
         exit(1);
     }
 
@@ -16,11 +20,12 @@ void listInsert(LIST *List, double key) {
     List->head = newNode;
 }
 
-// Function to append a new node with the given key at the tail of the list
+// Append a NEW node with the key at the tail of List
 void listAppend(LIST *List, double key) {
+    // allocate memory for newNode
     NODE *newNode = (NODE *)malloc(sizeof(NODE));
     if (!newNode) {
-        std::cerr << "Error: malloc failed" << std::endl;
+        fprintf(stderr, "Error: malloc failed\n");
         exit(1);
     }
 
@@ -35,5 +40,28 @@ void listAppend(LIST *List, double key) {
             pNODE = pNODE->next;
         }
         pNODE->next = newNode;
+    }
+}
+// Delete a node with the key from List
+void listDelete(LIST *List, double key) {
+    NODE *curr = List->head;
+    NODE *prev = NULL;
+
+    // Search for the node with key
+    while (curr != NULL) {
+        if (curr->key == key) {
+            // Node with key found
+            if (prev == NULL) {
+                // Key at Head
+                List->head = curr->next;
+            } else {
+                // Key elsewhere in List
+                prev->next = curr->next;
+            }
+            free(curr);
+            return;
+        }
+        prev = curr;
+        curr = curr->next;
     }
 }
