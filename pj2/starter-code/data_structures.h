7a0c7970eabb32a5c329adb4639f6bd88f6f32b7
Cheyenne C Swasey <cheysway1.git@purl.anonaddy.com>
2023-10-29 09:54:27 -0700

    init fr

---
+++ pj2/starter-code/data_structures.h
@@ -0,0 +1,16 @@
+#ifndef _data_structures_h
+#define _data_structures_h 1
+
+typedef struct TAG_ELEMENT {
+    double key;
+    // other fields as you see fit
+}ELEMENT;
+
+typedef struct TAG_HEAP {
+    int capacity; /* capacity of the heap */
+    int size; /* current size of the heap */
+    ELEMENT** A; /* array of pointers to ELEMENT */
+    // other fields as you see fit
+}HEAP;
+
+#endif
