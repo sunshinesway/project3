0b309e2ad8808482e53de8da04bd67af57e39ef4
Cheyenne C Swasey <cheysway1.git@purl.anonaddy.com>
2023-11-02 20:00:46 -0700

    first run of heap functions done

--- pj2/heap.cpp
+++ pj2/heap.cpp
@@ -35,63 +35,62 @@ void min_heapify(HEAP *heap, int elementIndex) {
 void build_min_heap(HEAP *heap) {
     heap->size = sizeof(heap->A) / sizeof(ELEMENT);
     for (int i = 0; i <= (heap->size/2; i++)) {
-        min_heapiy(heap, i);
+        min_heapify(heap, i);
     }
 }
 
 void heapsort(HEAP *heap) {
     build_min_heap(heap);
-    for (int i = )
+    int arrayLength = heap->size;
+    for (int i = 0; i < arrayLength; i++) {
+        swap(heap, 0, i);
+        heap->size--;
+        min_heapify(heap, i);
+    }
 
 }
-//HEAPSORT
-heapsort(A) {
-        build_max_heap(A);
-        for i = length(A) to 2
-        SWAP A[1], A[i];
-        heap-size[A]--;
-        max_heapify(A,1);
+int heap_min(HEAP *heap) {
+    return heap->A[0]->key;
+}
+int heap_extract_min(HEAP *heap){
+    if (heap->size < 1) {
+        fprintf(stderr, "Error: heap empty\n");
+        return 99999999;
+    }
+    else {
+        int minimum = heap->A[0]->key;
+        heap->A[0] = heap->A[heap->size];
+        heap->size--;
+        min_heapify(heap, 0);
+        return minimum;
+    }
 }
 
-//HEAP-MAXIMUM
-heap_max(A) {
-        return A[1];
+void heap_decrease_key(HEAP *heap, int elem, int key) {
+    if (key < heap->A[elem])
+        fprintf(stderr, "Error: heap empty\n");
+    else {
+        heap->A[elem]->key = key;
+
+        int parent = elem/2;
+        while(elem > 1 && heap->A[parent]->key > heap->A[elem]->key ) {
+            swap(heap, parent, elem);
+            elem = parent;
+        }
+    }
 }
 
-//HEAP-EXTRACT-MAX
-heap_extract_max(A) {
-        if heap-size[A] < 1
-        "error: heap empty"
-        else
-        max = A[1];
-        A[1] = A[heap-size[A]];
-        heap-size[A]--;
-        max_heapify(A,1);
-        return max;
-}
+void min_heap_insert(HEAP *heap, int key) {
+    heap->size++;
+    int i = heap->size;
 
-//INCREASE-KEY
-heap_increase_key(A,i,key) {
-if key < A[i]
-"error: new key smaller than current"
-else
-A[i] = key;
-while i > 1 && A[PARENT(i)] < A[i]
-SWAP A[i], A[PARENT(i)];
-i = PARENT(i);
-}
+    heap->A[i]->key = -2147483648;
+    heap_decrease_key(heap, i, key);
 
-//INSERTION
-max_heap_insert(A,key) {
-heap-size[A]++;
-i = heap-size[A];
-
-A[i] = -INFINITY;
-heap_increase_key(A,i,key);
 }
 
 
-
+/*
 //MAX-HEAPIFY
 max_heapify(A,i) {
 l = Left(i);
@@ -113,3 +112,45 @@ build_max_heap(A) {
         for i = ((length(A)/2)) to 1
         max_heapify(A,i);
 }
+//HEAPSORT
+heapsort(A) {
+        build_max_heap(A);
+        for i = length(A) to 2
+        SWAP A[1], A[i];
+        heap-size[A]--;
+        max_heapify(A,1);
+}
+//HEAP-MAXIMUM
+heap_max(A) {
+        return A[1];
+}
+//HEAP-EXTRACT-MAX
+heap_extract_max(A) {
+        if heap-size[A] < 1
+        "error: heap empty"
+        else
+        max = A[1];
+        A[1] = A[heap-size[A]];
+        heap-size[A]--;
+        max_heapify(A,1);
+        return max;
+}
+//INCREASE-KEY
+heap_increase_key(A,i,key) {
+if key < A[i]
+"error: new key smaller than current"
+else
+A[i] = key;
+while i > 1 && A[PARENT(i)] < A[i]
+SWAP A[i], A[PARENT(i)];
+i = PARENT(i);
+}
+//INSERTION
+max_heap_insert(A,key) {
+heap-size[A]++;
+i = heap-size[A];
+
+A[i] = -INFINITY;
+heap_increase_key(A,i,key);
+}
+ */
--- pj2/heap.h
+++ pj2/heap.h
@@ -3,3 +3,8 @@
 
 void min_heapify(HEAP *heap, int elementIndex);
 void build_min_heap(HEAP *heap);
+void heapsort(HEAP *heap);
+int heap_min(HEAP *heap);
+int heap_extract_min(HEAP *heap);
+void heap_decrease_key(HEAP *heap, int elem, int key);
+void min_heap_insert(HEAP *heap, int key);
